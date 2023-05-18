3e482efaf676448abfcafbe8f918399448211577
Saif <saifad303@gmail.com>
2023-05-18 18:53:47 +0600

    built: trusted company section. Home page.

---
+++ src/components/TrustedCompany/TrustedCompany.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const TrustedCompany = () => {
+  return (
+    <div className="py-14">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="mt-6">
+          <ul className="flex gap-y-6 flex-wrap items-center justify-center [&>*]:px-12 lg:divide-x">
+            {/* LOGO 1 */}
+            <li className="flex-none">
+              <img src="/img/barbie.png" alt="" className=" w-36" />
+            </li>
+
+            {/* LOGO 2 */}
+            <li className="flex-none">
+              <img src="/img/lego-icon.png" alt="" className=" w-36" />
+            </li>
+
+            {/* LOGO 3 */}
+            <li className="flex-none">
+              <img src="/img/polly-pocket.png" alt="" className=" w-36" />
+            </li>
+
+            {/* LOGO 4 */}
+            <li className="flex-none">
+              <img src="/img/hot-wheels.png" alt="" className=" w-36" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrustedCompany;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,17 @@ import Banner from "../components/Banner/Banner";
 import Gallery from "../components/Gallery/Gallery";
 import ShopByCategory from "../components/ShopByCategory/ShopByCategory";
 import Services from "../components/Services/Services";
+import TrustedCompany from "../components/TrustedCompany/TrustedCompany";
+TrustedCompany;
 
 const HomePage = () => {
   return (
     <div>
       <Banner></Banner>
       <Gallery></Gallery>
-      <ShopByCategory></ShopByCategory>
       <Services></Services>
+      <ShopByCategory></ShopByCategory>
+      <TrustedCompany></TrustedCompany>
     </div>
   );
 };
